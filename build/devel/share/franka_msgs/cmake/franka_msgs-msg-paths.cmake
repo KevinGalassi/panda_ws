5e13c34f8b16c55e15b23d79fbc4bf31e77ee937
KVN <galassi.kevin@gmail.com>
2020-11-23 21:18:53 +0100

    Remove errors

--- build/devel/share/franka_msgs/cmake/franka_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(franka_msgs_MSG_INCLUDE_DIRS "/home/kevin/ros/panda_ws/src/franka_ros/franka_msgs/msg;/home/kevin/ros/panda_ws/src/build/devel/share/franka_msgs/msg")
-set(franka_msgs_MSG_DEPENDENCIES std_msgs;actionlib_msgs)
--- build/tactile_sensor/read_sensor/catkin_generated/read_sensor-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(read_sensor_MESSAGE_FILES "/home/kevin/ros/panda_ws/src/tactile_sensor/read_sensor/msg/proximity_sensor_data.msg;/home/kevin/ros/panda_ws/src/tactile_sensor/read_sensor/msg/tactile_sensor_data.msg")
-set(read_sensor_SERVICE_FILES "")
